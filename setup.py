407f782dc4cad15ecae87a498d9745fae9304e39
Sosuke Kobayashi <soskek@users.noreply.github.com>
2021-12-21 13:16:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="glide-text2im",
-    packages=["glide_text2im"],
+    packages=["glide_text2im", "glide_text2im.tokenizer", "glide_text2im.clip"],
     install_requires=[
         "Pillow",
         "attrs",
